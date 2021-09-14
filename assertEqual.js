372ecf7e8bdee8facb6b339dc2c56bb91f7de2bf
jessonziegler <jziegleraudio@gmail.com>
2021-09-14 18:54:01 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('EQUAL!!');
+    return true;
+  } else {
+    console.log('NOT EQUAL!!!');
+    return false;
+  }
+};
+
+
