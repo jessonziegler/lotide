febdee09e910f5d68dd4cd56dde943df2f211cd7
jessonziegler <jziegleraudio@gmail.com>
2021-09-23 23:52:19 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,21 +1,6 @@
-// 2 for actual //
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('EQUAL!!');
-    return true;
-  } else {
-    console.log('NOT EQUAL!!!');
-    return false;
-  }
-};
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
-console.log(tail([1,2,3]));
-assertEqual(tail([1,2,3])[0], 2);
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(words);
+
+module.exports = tail;
