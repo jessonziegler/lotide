a390588c5d25dcee6e6d5e31ee7deb29925ce435
jessonziegler <jziegleraudio@gmail.com>
2021-09-23 23:54:09 +0000

    cleaned up

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-
-
-
 const head = function(array) {
-  // ! === falsy
-  // 0 === falsy
-  // falsy = undefined, null, 0, '', NaN
-  // truthy
+
   if (array.length === 0) {
     return undefined;
   } else {
