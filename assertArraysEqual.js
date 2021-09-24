5bba1f29281bcabb4611f1f932b90ac85bea8db3
jessonziegler <jziegleraudio@gmail.com>
2021-09-24 00:54:55 +0000

    refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
+const eqArrays = require('./eqArrays');
 
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 const assertArraysEqual = function(array1, array2){
   if (eqArrays(array1, array2)){
     console.log("equal")
@@ -18,5 +8,6 @@ const assertArraysEqual = function(array1, array2){
   }
 }
 assertArraysEqual([1,4,3],[1,2,3])
+module.exports = assertArraysEqual;
 
 
